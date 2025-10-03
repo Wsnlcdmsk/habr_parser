e51749aade0c9e427c2623b9726743faaec15d90
Wsnlcdmsk <pertovtim@gmail.com>
2025-10-03 03:56:26 +0300

    Write docker-compose and fix .gitignore.

--- parser_api/habr_parser/main.py
+++ parser_api/habr_parser/main.py
@@ -11,29 +11,29 @@ from habr_parser.services.scraper import get_daily_articles
 import logging
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     """Startup/shutdown context for FastAPI."""
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Startup/shutdown context for FastAPI."""
 
-#     async def scraper_loop():
-#         while True:
-#             try:
-#                 print("Fetching articles from Habr...")
-#                 await get_daily_articles(BASE_URL, NUM_PAGE)
-#                 print("Done! Sleeping...")
-#             except Exception as e:
-#                 print(f"Scraper error: {e}")
-#             await asyncio.sleep(INTERVAL)
+    async def scraper_loop():
+        while True:
+            try:
+                print("Fetching articles from Habr...")
+                await get_daily_articles(BASE_URL, NUM_PAGE)
+                print("Done! Sleeping...")
+            except Exception as e:
+                print(f"Scraper error: {e}")
+            await asyncio.sleep(INTERVAL)
 
-#     logging.basicConfig()
-#     logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
-#     task = asyncio.create_task(scraper_loop())
-#     yield
-#     task.cancel()
-#     try:
-#         await task
-#     except asyncio.CancelledError:
-#         print("Scraper task cancelled.")
+    logging.basicConfig()
+    logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
+    task = asyncio.create_task(scraper_loop())
+    yield
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("Scraper task cancelled.")
 
-app = FastAPI(title="Habr Scraper API")
+app = FastAPI(title="Habr Scraper API", lifespan = lifespan)
 app.include_router(routes.router)
